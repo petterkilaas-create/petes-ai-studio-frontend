cbf4848c90d6dafb958eafa9f8738f700b4fc472
petterkilaas-create <petter.kilaas@diakrit.com>
2026-03-29 15:39:23 +0000

    Endret til middleware og lagt inn env-nøkler i Vercel

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+// Vi definerer hvilke ruter som skal være ÅPNE (f.eks. selve innloggingssiden)
+// Siden vi vil låse ALT, lar vi denne være tom for øyeblikket, 
+// eller vi kan legge til spesifikke ruter hvis vi lager en "Landing page" senere.
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
+    // Denne linjen sørger for at dørvakten sjekker alle sider, 
+    // men ignorerer interne filer som bilder og scripts.
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
