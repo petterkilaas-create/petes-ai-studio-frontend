3c37268d38c5101ce63c60b1135a9c81547062d3
petterkilaas-create <petter.kilaas@diakrit.com>
2026-03-29 16:38:55 +0000

    Fikser hermetegn i supabaseClient

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://swookcccpytiyelmgqbd.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_pMSYiqQtFxZPbwJNC50XRg_kKqwGua7'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
